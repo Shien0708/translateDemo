bd053745e8bd4f198acc867c001ecfa610928b17
Shien <f0928652413@gmail.com>
2022-01-22 19:58:17 +0800

    Initial Commit

---
+++ translateDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  translateDemo
+//
+//  Created by 方仕賢 on 2022/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
